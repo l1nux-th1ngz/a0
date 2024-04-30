ec62f884e67b0096048255325da0a934b953c0ea
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-04-30 09:53:23 -0600

    Update slim.sh

--- slim.sh
+++ slim.sh
@@ -6,15 +6,15 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Check if the user 'slim' exists on the system
-if id "slim" &>/dev/null; then
-    echo "User 'slim' exists. Updating sudoers file..."
+# Check if the user 'savage' exists on the system
+if id "savage" &>/dev/null; then
+    echo "User 'savage' exists. Updating sudoers file..."
 else
-    echo "User 'slim' does not exist. Exiting..."
+    echo "User 'savage' does not exist. Exiting..."
     exit 1
 fi
 
-# Add a line to the sudoers file that allows 'slim' to run all commands without a password
-echo 'slim ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
+# Add a line to the sudoers file that allows 'savage' to run all commands without a password
+echo 'savage ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
 
 echo "Operation completed successfully."
