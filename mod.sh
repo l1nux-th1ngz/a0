29c954eec7d6cd69a2eb91e09658519693ed8600
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-05-04 03:14:44 -0600

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# File selection
+# Use Zenity to create a file selection dialogue with specified width and height
 DIRECTORY=$(zenity --file-selection --directory --width 650 --height 400)
 
-# C Select directory
+# Check if the user selected a directory
 if [ -n "$DIRECTORY" ]; then
     # Change the permissions of the selected directory
     chmod 777 "$DIRECTORY"
-    # Display message
-    zenity --info --text="Permissions for $DIRECTORY have been changed to 777."
+    # Display a success message
+    zenity --info --text="Permissions for $DIRECTORY have been changed to 777." --width 650 --height 400
 else
     # Display an error message if no directory was selected
-    zenity --error --text="No directory selected. Please try again."
+    zenity --error --text="No directory selected. Please try again." --width 650 --height 400
 fi
