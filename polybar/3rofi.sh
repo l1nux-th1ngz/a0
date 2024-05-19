7d56eafb72f7afc308d6f0859cbab9a01df15c9c
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-05-19 01:23:26 -0600

    Add files via upload

---
+++ polybar/3rofi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install required packages
+sudo apt install -y rofi dmenu polybar
+
+# Instructions for setting up keyboard shortcuts
+echo "Installation complete. Please set up the following shortcuts manually:"
+echo
+echo "1. Rofi launcher: rofi -show drun"
+echo "2. dmenu launcher: dmenu_run"
+echo "Shortcut for Rofi: Super + Space"
+echo "Shortcut for dmenu: Customize as needed (e.g., Super + Shift + Space)"
---
+++ polybar/sys-upgrade.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+alacritty -e sh -c "sudo apt-get update && sudo apt-get-upgrade -y"
